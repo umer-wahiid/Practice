7fb8cc0c608ac296a7fc9aae21531702e7757f99
Umer Abdul Wahid <umerwahiid48@gmail.com>
2023-03-30 14:31:47 +0500

    Department Started

---
+++ Practice2/Practice2/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Practice2.Models
+{
+    public class Department
+    {
+        [Key]
+        [ForeignKey("Employee")]
+        public int DId { get; set; }
+
+        [StringLength(50)]
+        public string DName { get; set; }
+
+        public int EmployeeId { get; set; }
+        public virtual Employee Employees { get; set; }
+    }
+}
